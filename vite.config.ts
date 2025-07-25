9f40a3aa806c0ca7d8d13be2d8e8aabd3e3a1072
spiral023 <asi@liwest.at>
2025-07-25 02:28:29 +0200

    Adds chunk size limit and manual chunking for vendor files
    Implements a chunk size warning limit of 1000 for builds.
    
    Introduces manual chunking to categorize dependencies from node_modules into a separate vendor file, enhancing build performance and organization.

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,16 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 }));
